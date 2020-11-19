13ee1f55f61b43cdf499dcc8fa3b54c023b33a23
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-11-19 17:03:51 -0600

    Readding deleted files

---
+++ Assets/Scripts/HUD/InventorySlot.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    [SerializeField]
+    private Image icon;
+    [SerializeField]
+    private Button removeButton;
+    private Item item;
+
+    public void AddItem(Item newItem)
+    {
+        item = newItem;
+
+        icon.sprite = item.Icon;
+        icon.enabled = true;
+        removeButton.interactable = true;
+    }
+
+    public void ClearSlot()
+    {
+        item = null;
+
+        icon.sprite = null;
+        icon.enabled = false;
+        removeButton.interactable = false;
+    }
+
+    public void UseItem()
+    {
+        if(item != null)
+        {
+            item.Use();
+        }
+    }
+
+    public void OnRemoveButton()
+    {
+        Inventory.instance.Remove(item);
+    }
+}
---
+++ Assets/Scripts/Helper/ConvertToRegularMesh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertToRegularMesh : MonoBehaviour
+{
+    // Convert skinned mesh renderer to regular mesh renderer, use for rendering equipment in environments.
+    // Add Convert To Regular Mesh component to object with Skinned Mesh Renderer.
+    // From the component's context menu, choose 'Convert to Regular Mesh'.
+
+    [ContextMenu("Convert to Regular Mesh")]
+    void Convert()
+    {
+        SkinnedMeshRenderer skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+
+        meshFilter.sharedMesh = skinnedMeshRenderer.sharedMesh;
+        meshRenderer.sharedMaterials = skinnedMeshRenderer.sharedMaterials;
+
+        DestroyImmediate(skinnedMeshRenderer);
+        DestroyImmediate(this);
+    }
+}
